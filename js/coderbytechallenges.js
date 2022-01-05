2538cb89665d93ee5820515124531a5f574921d9
Ali Kartal <ali_kartal91@hotmail.com>
2022-01-05 20:36:02 +0300

    coderbytechallange.js "First Reverse"

--- js/coderbytechallenges.js
+++ js/coderbytechallenges.js
@@ -12,9 +12,38 @@ function FirstFactorial(num) {
 console.log(FirstFactorial(8)); 
 
 
+// ******** FIRST REVERSE ********
+
+// Method - 1:
+function FirstReverse(str){
+    var backwardString = "";
+    var splitString = str.split("");
+    for (var i = splitString.length - 1; i >= 0; i--){
+        backwardString += splitString[i];
+    }
+    return backwardString;
+}
+//console.log(FirstReverse("coderbyte")); // etybredoc
+
+
+
+// Method - 2:
+function FirstReverse(str) { 
+
+    // code goes here  
+    return str.split('').reverse().join(''); 
+           
+  }
+     
+  // keep this function call here 
+  FirstReverse(readline());
 
 
 
 
 
 
+
+
+
+  
