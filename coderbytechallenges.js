634f2c4df1265182ac6fa7c06920e006e154ae71
Ali Kartal <ali_kartal91@hotmail.com>
2022-05-02 13:04:19 +0300

    "Two Sum" added

--- coderbytechallenges.js
+++ coderbytechallenges.js
@@ -1194,3 +1194,46 @@ console.log(LetterChanges("abcdefghijklmnopqrstuvwxyz")) // bcdEfghIjklmnOpqrstU
 
 
 
+
+
+
+
+// ******** Two Sum  ********
+
+function TwoSum(arr) {
+
+    let res = []
+
+    for (let i = 1; i < arr.length; i++) {
+        for (let x = i + 1; x < arr.length; x++) {
+            if (arr[i] + arr[x] === arr[0]) {
+                res.push(`${arr[i]},${arr[x]}`)
+                // console.log(res)
+            }
+        }
+    }
+
+    if (res.length !== 0) {
+        return res.join(" ")
+    } else {
+        return -1
+    }
+
+}
+console.log(TwoSum([7, 3, 5, 2, -4, 8, 11])) // 5,2 -4,11
+console.log(TwoSum([17, 4, 5, 6, 10, 11, 4, -3, -5, 3, 15, 2, 7])) // 6,11 10,7 15,2
+console.log(TwoSum([7, 6, 4, 1, 7, -2, 3, 12])) // 6,1 4,3
+
+console.log(TwoSum([100, 90, 90, 90, 90, 11])) // -1
+console.log(TwoSum([6, 2])) // -1
+console.log(TwoSum([4, 5, 2, 1])) // -1
+
+
+
+
+
+
+
+
+
+
